fd5d3d06f01d05c9aa8fc2fb8c9a8e8974259733
Sebastian Freundt <freundt@ga-group.nl>
2026-02-20 16:09:40 +0000

    provide alignments

--- sql/load-region-align.sql
+++ sql/load-region-align.sql
@@ -1,3 +1,7 @@
 SET u{GRAPH} http://data.ga-group.nl/region.align/;
 SET u{FILE} /home/freundt/author/region/region-align.ttl;
 LOAD '/home/freundt/author/region/sql/load-generic.sql';
+
+SET u{PREDx} dct:replaces;
+SET u{PREDy} dct:isReplacedBy;
+LOAD '/home/freundt/author/region/sql/infer-props.sql';
